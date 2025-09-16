977344ebdd533467da2c432eafda411af0e8ed9a
David Hunt <git@dnotes.net>
2025-09-17 06:19:04 +1200

    removed old social icons from the exports too

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -15,13 +15,3 @@ export { default as ForgottenPasswordView } from './views/ForgottenPasswordView.
 export { default as MFAChallengeView } from './views/MFAChallengeView.svelte'
 export { default as AddMFAView } from './views/AddMFAView.svelte'
 export { default as SocialAuthView } from './views/SocialAuthView.svelte'
-
-// Social icons
-export { default as AzureIcon } from './social-icons/AzureIcon.svelte'
-export { default as BitbucketIcon } from './social-icons/BitbucketIcon.svelte'
-export { default as DiscordIcon } from './social-icons/DiscordIcon.svelte'
-export { default as FacebookIcon } from './social-icons/FacebookIcon.svelte'
-export { default as GithubIcon } from './social-icons/GithubIcon.svelte'
-export { default as GitlabIcon } from './social-icons/GitlabIcon.svelte'
-export { default as GoogleIcon } from './social-icons/GoogleIcon.svelte'
-export { default as TwitterIcon } from './social-icons/TwitterIcon.svelte'
