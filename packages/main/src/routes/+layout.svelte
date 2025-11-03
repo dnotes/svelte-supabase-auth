50f69457ffd41477434283b1e548de6c94a531f6
David <git@dnotes.net>
2025-11-03 19:34:49 +1300

    fix(site): color of top links

--- packages/main/src/routes/+layout.svelte
+++ packages/main/src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 </svelte:head>
 
 <div class="w-screen h-screen overflow-hidden bg-stone-50 dark:bg-stone-950 text-black dark:text-white flex flex-col items-center">
-	<header class="h-12 flex-shrink-0 flex gap-4 items-center justify-end w-full px-5 bg-stone-200 dark:bg-stone-800 text-stone-300 dark:text-stone-700">
+	<header class="h-12 flex-shrink-0 flex gap-4 items-center justify-end w-full px-5 bg-stone-200 dark:bg-stone-800 text-stone-800 dark:text-stone-200">
 		<a href="https://github.com/dnotes/svelte-supabase-auth/tree/main/packages/main"><GithubIcon width="32" height="32" /></a>
 		<a href="https://npmjs.com/package/svelte-supabase-auth"><NpmIcon width="32" height="32" /></a>
 	</header>
