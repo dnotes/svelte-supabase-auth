f1e8a8ab8d02fb7118cb3735ac0fe3c1985d7a18
David <git@dnotes.net>
2026-01-16 12:00:43 +1300

    fix: axe errors in site layout

--- packages/main/src/routes/+layout.svelte
+++ packages/main/src/routes/+layout.svelte
@@ -16,9 +16,9 @@
 		<a href="https://github.com/dnotes/svelte-supabase-auth/tree/main/packages/main"><GithubIcon width="32" height="32" /></a>
 		<a href="https://npmjs.com/package/svelte-supabase-auth"><NpmIcon width="32" height="32" /></a>
 	</header>
-	<div class="flex-grow overflow-auto p-6 md:p-16 w-full">
+	<main class="flex-grow overflow-auto p-6 md:p-16 w-full">
 		{@render children()}
-	</div>
+	</main>
 	<footer class="h-8 flex-shrink-0 flex gap-4 items-center justify-end w-full px-5 text-xs bg-stone-200 dark:bg-stone-800 text-stone-600 dark:text-stone-400">
 		<a href="https://npmjs.com/package/svelte-supabase-auth">Svelte Supabase Auth v{version}</a>
 		<div class="flex-grow"></div>
@@ -28,11 +28,3 @@
 		<a href="/terms">terms</a>
 	</footer>
 </div>
-
-<style lang="postcss">
-  @reference "tailwindcss";
-
-  footer a {
-    @apply text-sky-400 dark:text-sky-600;
-  }
-</style>
